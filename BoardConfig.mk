58f701737012d1a680d39cc43cd6f7a679ac6d99
firebird11 <hbgassel@gmail.com>
2018-08-22 08:21:25 +0200

    raphael: Enable sdclang
    Change-Id: I55df3fa378bce8374b8ff2490f97253ee112cff4
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ BUILD_BROKEN_DUP_RULES := true
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
+# Use Snapdragon LLVM, if available
+TARGET_USE_SDCLANG := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
