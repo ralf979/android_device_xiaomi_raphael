15c4aebdf0c9c380fab9e4d2e462812f57527b2f
PIPIPIG233666 <pig.priv@gmail.com>
2019-08-24 09:04:45 -0400

    raphael: light: Adapt for our blue LED light
    Change-Id: I7f1b5aa8f6b92c62c51a661594d1cdea0c2878a2

--- light/Light.cpp
+++ light/Light.cpp
@@ -23,8 +23,11 @@
 #include <fstream>
 
 #define LCD_LED         "/sys/class/backlight/panel0-backlight/"
+#define BLUE_LED        "/sys/class/leds/green/"
 
 #define BRIGHTNESS      "brightness"
+#define DELAY_OFF       "delay_off"
+#define DELAY_ON        "delay_on"
 
 #define MAX_LED_BRIGHTNESS    255
 #define MAX_LCD_BRIGHTNESS    2047
@@ -87,6 +90,25 @@ static void handleBacklight(const LightState& state) {
         brightness = 2;
     }
     set(LCD_LED BRIGHTNESS, brightness);
+}
+
+static void handleNotification(const LightState& state) {
+    uint32_t blueBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
+
+    /* Disable breathing or blinking */
+    set(BLUE_LED BREATH, 0);
+    set(BLUE_LED BRIGHTNESS, 0);
+
+    if (state.flashMode == Flash::TIMED) {
+        /* Blue */
+        set(BLUE_LED DELAY_OFF, state.flashOnMs);
+        set(BLUE_LED DELAY_ON, state.flashOffMs);
+
+        /* Enable Breathing */
+        set(BLUE_LED BREATH, 1);
+    } else {
+        set(BLUE_LED BRIGHTNESS, blueBrightness);
+    }
 }
 
 static inline bool isStateLit(const LightState& state) {
