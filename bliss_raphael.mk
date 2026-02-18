b6de62ff58868ff523c9436e4387c4a4c25fab2b
ralf979 <ralf979@gmail.com>
2026-02-18 11:07:30 +0100

    raphael: set boot animation resolution

--- bliss_raphael.mk
+++ bliss_raphael.mk
@@ -14,6 +14,9 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := bliss_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
